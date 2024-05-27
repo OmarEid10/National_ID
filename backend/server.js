287b04aab647abda08758f7c6e253d1f2de35a63
OmarEid10 <omareed541@gmail.com>
2024-05-27 19:34:11 +0300

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import nationalIDRoutes from "./routes/nationalID.routes.js";
 
@@ -8,14 +9,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 
 app.use("/api/", nationalIDRoutes);
 
